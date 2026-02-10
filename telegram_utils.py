7224eaf856f4a72e9603324828303246e9963f74
qwl <4vlad.ilyin@gmail.com>
2026-02-10 21:17:44 +0300

    added FSM on app_key:app_secret

--- telegram_utils.py
+++ telegram_utils.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 from aiogram.filters import CommandStart, Command
 from co2_detector import co2_status
 from checker import check_status
@@ -16,6 +18,9 @@ BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+
+class Form(StatesGroup):
+    APP_KEY_SECRET = State()
 
 @dp.message(CommandStart())
 async def start_handler(message: Message):
@@ -40,7 +45,16 @@ async def start_check(message: Message):
     )
     await message.answer(f"Началась постоянная проверка CO2 в комнате. Сейчас у вас целых {level_co2} ppm!")
 
+@dp.message(Command("set_key"))
+async def set_key(message: Message, state:FSMContext):
+    await message.answer("Введите в таком формате: APP_SECRET:APP_KEY")
+    await state.set_state(Form.APP_KEY_SECRET)
 
+@dp.message(Form.APP_KEY_SECRET)
+async def get_key(message: Message, state: FSMContext):
+    text = message.text  
+    await message.answer(f"Ты ввел: {text}")
+    await state.clear()
 
 async def main():
     scheduler.start()
